c245cc3092b1da822aaf407f1b7b30fb16cd4a70
tommygorham <tommy.gorham97@gmail.com>
2025-07-19 20:55:32 -0700

    Initial commit for purchase matcher
    scraper.py and analyzer.py now focus on purchases
    More organized data generation from run.sh

--- README.md
+++ README.md
@@ -2,30 +2,8 @@
 MacOS web scraper/CLI Tool to get recent stock acticity 
 
 ### Description
-Python Web Scraper, csv/png generator 
+This is an offshoot of the main branch where we focus on purchases. 
 
-### Usage
-```
-./run.sh 
-```
-### Output
-```
-congress_trading_data.csv
-congress_trading_distribution.png
-congress_trading_net_activity.png
-congress_trading_top_purchases.png
-congress_trading_top_sales.png
+The idea here is to match recent stock analyst upgrades
+& insider, congressional purchases to institutional purchases
 
-insider_trading_data.csv
-insider_trading_distribution.png
-insider_trading_net_activity.png
-insider_trading_top_purchases.png
-insider_trading_top_sales.png
-```
-
-### Contributing 
-Submit a pull request with the powershell version of ./run.sh. 
-
-That way this can be supported on Windows. 
-
-_Word of wisdom_: Less code is better code
--- run.sh
+++ run.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-cd scripts
-
-# Create a directory to store today's analysis with time in data/qq
-ANALYSIS_DIR="../data/qq/$(date +"%m-%d-%Y_%H-%M-%S")"
+cd scripts/
+# Create a directory to store today's analysis in data/
+ANALYSIS_DIR="../data/$(date +"%m-%d-%Y")"
 mkdir -p "$ANALYSIS_DIR"
 
-python zacks.py 
+# Get current time for file naming
+TIMESTAMP=$(date +"%H-%M-%S")
+
+# Update user about zacks new #1 additions
+python zacks.py
 
 # Run insider trading analysis pipeline
 python scrape.py insider | python analyzer.py insider
-
 # Check if insider analysis was successful
 if [ $? -eq 0 ]; then
-  # Move insider files to the analysis directory
-  mv insider_trading*.png "$ANALYSIS_DIR/" 2>/dev/null
-  mv insider_trading_data.csv "$ANALYSIS_DIR/" 2>/dev/null
+  # Move insider files to the analysis directory with timestamp
+  mv insider_trading_data.csv "$ANALYSIS_DIR/insider_trading_data_${TIMESTAMP}.csv" 2>/dev/null
 else
   echo "Error: insider analysis failed."
   exit 1
@@ -22,12 +23,10 @@ fi
 
 # Run congress trading analysis pipeline
 python scrape.py congress | python analyzer.py congress
-
 # Check if congress analysis was successful
 if [ $? -eq 0 ]; then
-  # Move congress files to the analysis directory
-  mv congress_trading*.png "$ANALYSIS_DIR/" 2>/dev/null
-  mv congress_trading_data.csv "$ANALYSIS_DIR/" 2>/dev/null
+  # Move congress files to the analysis directory with timestamp
+  mv congress_trading_data.csv "$ANALYSIS_DIR/congress_trading_data_${TIMESTAMP}.csv" 2>/dev/null
 else
   echo "Error: Congress analysis failed."
   exit 1
